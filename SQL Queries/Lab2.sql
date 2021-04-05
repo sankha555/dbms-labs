4972a75abd5cd7d849c959cb7994188c31e296c0
sankha555 <f20190029@pilani.bits-pilani.ac.in>
2021-04-05 20:10:09 +0530

    Lab 3

--- SQL Queries/Lab2.sql
+++ SQL Queries/Lab2.sql
@@ -99,3 +99,86 @@ FROM
 
 // b) 
 
+-- Q1 a
+SELECT s.ID, SUM(c.credits) as total_credits
+FROM (
+    SELECT ID, name FROM student
+    WHERE ID = 99289
+)s NATURAL JOIN takes t NATURAL JOIN course c;
+
+SELECT ID, SUM(credits*gp) as total_gp FROM (
+    SELECT 
+    s.ID, c.credits,  
+    CASE t.grade
+        WHEN "A " THEN 4.0 
+        WHEN "A-" THEN 3.7 
+        WHEN "B+" THEN 3.3 
+        WHEN "B " THEN 3.0 
+        WHEN "B-" THEN 2.7 
+        WHEN "C+" THEN 2.3 
+        WHEN "C " THEN 2.0 
+        WHEN "C-" THEN 1.7 
+    END as gp 
+    FROM (
+        SELECT ID, name FROM student
+        WHERE ID = 99289
+    )s NATURAL JOIN takes t NATURAL JOIN course c
+)gps;
+
+/*
++-------+----------+
+| ID    | total_gp |
++-------+----------+
+| 99289 |     77.5 |
++-------+----------+
+*/
+
+-- Q1 b
+SELECT ID, SUM(credits*gp)/SUM(credits) as cgpa FROM (
+    SELECT 
+    s.ID, c.credits,  
+    CASE t.grade
+        WHEN "A " THEN 4.0 
+        WHEN "A-" THEN 3.7 
+        WHEN "B+" THEN 3.3 
+        WHEN "B " THEN 3.0 
+        WHEN "B-" THEN 2.7 
+        WHEN "C+" THEN 2.3 
+        WHEN "C " THEN 2.0 
+        WHEN "C-" THEN 1.7 
+    END as gp 
+    FROM (
+        SELECT ID, name FROM student
+        WHERE ID = 99289
+    )s NATURAL JOIN takes t NATURAL JOIN course c
+)gps;
+
+/*
++-------+---------+
+| ID    | cgpa    |
++-------+---------+
+| 99289 | 2.03947 |
++-------+---------+
+*/
+
+-- Q1 c
+SELECT ID, SUM(credits*gp)/SUM(credits) as cgpa FROM (
+    SELECT 
+    s.ID, c.credits,  
+    CASE t.grade
+        WHEN "A " THEN 4.0 
+        WHEN "A-" THEN 3.7 
+        WHEN "B+" THEN 3.3 
+        WHEN "B " THEN 3.0 
+        WHEN "B-" THEN 2.7 
+        WHEN "C+" THEN 2.3 
+        WHEN "C " THEN 2.0 
+        WHEN "C-" THEN 1.7 
+        WHEN NULL THEN 0
+    END as gp 
+    FROM student s NATURAL JOIN takes t NATURAL JOIN course c
+)gps
+GROUP BY ID;
+
+-- Q2
+-- Same a Lab 5 Questions
